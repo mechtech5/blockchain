8e23a097fd5aadf71e91a36fbe9850a55b2b7d90
Ayush Likhar <neontechhead@gmail.com>
2022-01-23 20:11:17 +0530

    created constructor function

---
+++ dev/blockchain.js
@@ -0,0 +1,20 @@
+export const Blockchain = () => {
+  this.chain = [];
+  this.newTransactions = [];
+};
+
+Blockchain.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
+
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
