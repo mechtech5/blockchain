a13abc38e383f8d568d46e26ad4428f438ce0b57
Ayush Likhar <neontechhead@gmail.com>
2022-01-23 20:22:18 +0530

    added a test

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,20 +1,23 @@
-export const Blockchain = () => {
-  this.chain = [];
-  this.newTransactions = [];
+export class Blockchain {
+	constructor() {
+		this.chain = [];
+		this.newTransactions = [];
+	}
+	createNewBlock(nonce, previousBlockHash, hash) {
+		const newBlock = {
+			index: this.chain.length + 1,
+			timestamp: Date.now(),
+			transactions: this.newTransactions,
+			nonce: nonce,
+			hash: hash,
+			previousBlockHash: previousBlockHash,
+		};
+
+		this.newTransactions = [];
+		this.chain.push(newBlock);
+
+		return newBlock;
+	}
 };
 
-Blockchain.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
-  const newBlock = {
-    index: this.chain.length + 1,
-    timestamp: Date.now(),
-    transactions: this.newTransactions,
-    nonce: nonce,
-    hash: hash,
-    previousBlockHash: previousBlockHash,
-  };
 
-  this.newTransactions = [];
-  this.chain.push(newBlock);
-
-  return newBlock;
-};
--- dev/test.js
+++ dev/test.js
@@ -1,1 +1,7 @@
+import { Blockchain } from "./blockchain.js";
 
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(100, "HASH", "HASH");
+
+console.log(bitcoin);
